2c44e51b65a9bc0001b499ea51ecd6461d92fae5
Sinem Aytac <70482578+siaytac@users.noreply.github.com>
2025-05-07 09:15:49 -0400

    Update updatevectors.R

--- updatevectors.R
+++ updatevectors.R
@@ -1,9 +1,9 @@
 updatevectors <- function(Nf, gsys, studylistreordered, memorytraces, u, c, bestmatch.source, ii, trial, bestmatch) {
   
   randFeatures<-rgeom(Nf, gsys)+1 #Random features
-  probe = studylistreordered[(1+20*(ii-1)):(20*ii), trial] # probe including every feature stored correctly 
+  probe = studylistreordered[(1+Nf*(ii-1)):(Nf*ii), trial] # probe including every feature stored correctly 
   
-  trace <- array(memorytraces[(1+20*(bestmatch.source-1)):(20*bestmatch.source), bestmatch]) 
+  trace <- array(memorytraces[(1+Nf*(bestmatch.source-1)):(Nf*bestmatch.source), bestmatch]) 
   randStore <- array(runif(Nf), length(trace))
   randCorr <- array(runif(Nf), length(trace))
   storeWhich <- (randStore<=u & trace==0) # only if the feature equals to zero
@@ -11,7 +11,7 @@ updatevectors <- function(Nf, gsys, studylistreordered, memorytraces, u, c, best
   
   trace[storeWhich] <- randFeatures[storeWhich]
   trace[corrWhich] <- probe[corrWhich]
-  memorytraces[(1+20*(bestmatch.source-1)):(20*bestmatch.source), bestmatch] = trace
+  memorytraces[(1+Nf*(bestmatch.source-1)):(Nf*bestmatch.source), bestmatch] = trace
   
   return(memorytraces)
 }
