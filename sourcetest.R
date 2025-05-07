084784626cae964dbd0ca61ec44cb7a807b85c65
Sinem Aytac <70482578+siaytac@users.noreply.github.com>
2025-05-07 09:22:17 -0400

    Update sourcetest.R

--- sourcetest.R
+++ sourcetest.R
@@ -1,11 +1,11 @@
-testsourcememory <- function(memorytraces, testsource, c, gsys, n, bestmatch) {
+testsourcememory <- function(Nf, memorytraces, testsource, c, gsys, n, bestmatch) {
   
   sourcealllikes=matrix(NA, n, ncol=1)
   
   for(counttrace in 1:n){ #go through each source
     
     trace = matrix((memorytraces[, bestmatch]), ncol=1) #best matching trace source+item
-    trace = matrix(trace[(1+20*(counttrace)):(20*(counttrace+1))], ncol=1) #best matching trace source
+    trace = matrix(trace[(1+Nf*(counttrace)):(Nf*(counttrace+1))], ncol=1) #best matching trace source
     numzeros = sum((trace == 0))
     match = which(testsource == trace)  
     mismatch = which(testsource != trace) 
